4867bf691f414dd3b6180553e825bc03e06c4e1c
Baptiste Langlade <baptouuuu@gmail.com>
2019-12-14 12:29:19 +0100

    add fixtures

---
+++ fixtures/MediaType.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types = 1);
+
+namespace Fixtures\Innmind\MediaType;
+
+use Innmind\MediaType\{
+    MediaType as Model,
+    Parameter,
+};
+use Innmind\BlackBox\Set;
+use function Innmind\Immutable\unwrap;
+
+final class MediaType
+{
+    /**
+     * @return Set<Model>
+     */
+    public static function any(): Set
+    {
+        return Set\Composite::of(
+            static function($topLevel, $subType, $suffix, $parameterName, $parameterValue): Model {
+                if ($parameterName) {
+                    return new Model(
+                        $topLevel,
+                        $subType,
+                        $suffix,
+                        new Parameter(
+                            $parameterName,
+                            $parameterValue,
+                        ),
+                    );
+                }
+
+                return new Model(
+                    $topLevel,
+                    $subType,
+                    $suffix,
+                );
+            },
+            Set\Elements::of(...unwrap(Model::topLevels())),
+            Set\Strings::any(),
+            Set\Strings::any(),
+            new Set\Either(
+                Set\Strings::any(),
+                Set\Elements::of(null), // to generate a type without a parameter
+            ),
+            Set\Strings::any(),
+        );
+    }
+}
